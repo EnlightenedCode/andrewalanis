8831c6e778bf58ed1cef84f6723daffbfc5b1b12
Andrew Alanis <andrew4336@gmail.com>
2015-12-06 00:00:59 -0600

    add server.js back again

---
+++ server.js
@@ -0,0 +1,12 @@
+var PORT = process.env.PORT || 8080;
+var express = require("express");
+
+var app = express();
+app.use(app.router);
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.send("Hello, World from Andrew! Hello test3teasd again");
+});
+
+app.listen(PORT);
