0c7845dadf015bf24a54ea5cf79d3c505b2b4f7e
Andrew Alanis <andrew4336@gmail.com>
2015-12-07 10:34:00 -0600

    create auth on node server

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var PORT = process.env.PORT || 8080;
 var express = require("express");
+var auth = require('basic-auth');
 
 var app = express();
 app.use(app.router);
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.send("Hello, World from Andrew! Hello test THIS IS HOW YOU KNOW");
+    var credentials = auth(req);
+
+    if (!credentials || credentials.name !== 'develop' || credentials.pass !== 'developonme') {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+        res.end('Access denied')
+    } else {
+        res.send("Hello, You are in!");
+        res.end('Access granted');
+
+    }
+
 });
 
 app.listen(PORT);
