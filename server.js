94939b4fa8210202a317061d466150e7f8733eae
Andrew Alanis <andrew4336@gmail.com>
2015-12-10 19:52:46 -0600

    add credentials to env vars

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var PORT = process.env.PORT || 8080;
+var dokkuLogin = process.env.LOGIN_DOKKU;
+var dokkuHash = process.env.HASH_DOKKU;
 var express = require("express");
 var auth = require('basic-auth');
 
@@ -9,7 +11,7 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", function(req, res){
     var credentials = auth(req);
 
-    if (!credentials || credentials.name !== 'develop' || credentials.pass !== 'developonme') {
+    if (!credentials || credentials.name !== dokkuLogin || credentials.pass !== dokkuHash) {
         res.statusCode = 401;
         res.setHeader('WWW-Authenticate', 'Basic realm="example"');
         res.end('Access denied')
